ab80cfc41775e49eb8e887367f1338f94da0e8f0
Fachri Febrian <fachripaul@gmail.com>
2017-11-13 17:34:44 +0700

    Initial Commit

---
+++ Prelo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Prelo
+//
+//  Created by Fachri Work on 11/13/17.
+//  Copyright © 2017 Decadev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
